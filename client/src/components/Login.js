4277ecf92d2b5177ce30504859181f957d3e8313
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-18 21:49:37 -0400

    Login page styled

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,39 +1,70 @@
 import React from "react";
 import Logo from '../assets/akullabelka_logo.svg';
 import {useHistory} from "react-router-dom";
+import styled from "styled-components";
 
 const Login = () => {
     const history = useHistory();
 
     const handleLogin = (ev) => {
         ev.preventDefault();
-        history.push("/homepage");
+        history.push("/main");
     }
     const handleSignin = (ev)=>{
         ev.preventDefault();
         history.push("/create-user");
     }
     return (
-        <>
-        <img src={Logo} alt="akullabelka logo"/>
-        <form onSubmit ={handleLogin}>
-          <label>
+        <Container>
+        <LogoImage src={Logo} alt="akullabelka logo"/>
+        <LoginForm onSubmit ={handleLogin}>
+          <LabelLogin>
               Email:
-              <input type="email" name="email"/>
-          </label>
-          <label>
+              <InputEmail type="email" name="email"/>
+          </LabelLogin>
+          <LabelLogin>
               Password:
               <input type="password" name="password"/>
-          </label>
-          <input type="submit" value="Log In!"/>
-        </form>
-        <div>
+          </LabelLogin>
+          <InputLoginButton type="submit" value="Log In!"/>
+        </LoginForm>
+        <SigninDiv>
             <p>First Time here?</p>
             <button onClick={handleSignin} type="button">Sign In!</button>
-        </div>
-        
-        </>
+        </SigninDiv>
+        </Container>
     )
 };
+const Container = styled.div`
+  display:flex;
+  flex-direction: column;
+`;
+const LogoImage = styled.img`
+  border-radius: 50%;
+  background-color:lightgrey;
+  width:200px;
+  margin: 0 auto 10px;
+`;
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+const InputEmail = styled.input`
+  width:79%;
+`;
+const LabelLogin = styled.label`
+  margin: 10px 0;
+`;
+
+const InputLoginButton = styled.input`
+  margin: 10px 0;
+`
+const SigninDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+
+`
 
 export default Login;
