794b438e8addf8d0f91bb099eadc1c5ecd28d2ed
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-21 20:23:25 -0400

    added Links to move around the pages

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import MainHeader from '../reusable-components/MainHeader';
 import Container from '../reusable-components/Container';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   return (
@@ -9,13 +10,16 @@ const Homepage = () => {
       <MainHeader/>
         <div>
           <ul>
-            <li>Profile</li>
-            <li>Get a Loan</li>
-            <li>Transactions</li>
-            <li>Available Rates</li>
+            <StyledLink to="/main/profile">Profile</StyledLink>
+            <StyledLink to="/main/get-loan">Get a Loan</StyledLink>
+            <StyledLink to="/main/transactions">Transactions</StyledLink>
           </ul>
         </div>
     </Container>
-    )
-    };
+  )
+};
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #fff;
+`
 export default Homepage;
