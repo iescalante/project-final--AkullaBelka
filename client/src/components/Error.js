5e6a52f4dc88e9c66e8d087b01003d3ec331079b
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-25 15:20:20 -0400

    added link to go back to login for Error page

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import Logo from '../assets/akullabelka_logo.svg';
+import { useHistory } from 'react-router-dom';
 
 const Error = () => {
-    return (
-        <>
-        <h1>Hey....sorry there's an error somewhere</h1>
-        </>
+  const history = useHistory();
+  const handleGoLogin = () => {
+    history.push("/");
+  }
+  return (
+    <>
+      <h1>Hey....sorry there's an error somewhere</h1>
+      <LogoImage src={Logo} onClick={handleGoLogin} />
+      <h2>Click on Logo to go back to the login</h2>
+    </>
     )
 };
-
+const LogoImage = styled.img`
+  border-radius: 50%;
+  background-color: lightgrey;
+  width: 50px;
+  margin-right: 15px;
+  cursor: pointer;
+`;
 export default Error;
