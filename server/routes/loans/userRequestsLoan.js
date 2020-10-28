d0f6468fabb028ad16e2d035e9de48f4b13fffc6
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-28 19:29:23 -0400

    added the update for transactions with useful info

--- server/routes/loans/userRequestsLoan.js
+++ server/routes/loans/userRequestsLoan.js
@@ -1,4 +1,5 @@
 const { MongoClient, ObjectID } = require("mongodb");
+const { transactionAndDueDates } = require("../../utilities");
 require("dotenv").config();
 const { MONGO_URI } = process.env;
 
@@ -10,8 +11,8 @@ const options = {
 module.exports = async (req, res) => {
     const client = await MongoClient(MONGO_URI, options);
     const { userId, lenderId } = req.params;
-    const { loanAmount } = req.body;
-
+    const { loanAmount, selectedRate, daysToPay } = req.body;
+    
     try {
         await client.connect();
         const db = client.db("akulla_belka");
@@ -20,12 +21,24 @@ module.exports = async (req, res) => {
         await db.collection("users").findOneAndUpdate({ _id: ObjectID(userId) },{$inc: {totalLoaned: loanAmount}});
         await db.collection("users").findOneAndUpdate({_id: ObjectID(lenderId)}, {$inc:{"lenderProfile.totalLoan": loanAmount}});
         await db.collection("users").findOneAndUpdate({_id: ObjectID(lenderId)}, {$push:{"lenderProfile.usersId": {userId, loanAmount}}});
+        await db.collection("transactions").insertOne({
+          userId,
+          transactionDate: transactionAndDueDates(daysToPay).transactionDate,
+          loanAmount,
+          lenderId,
+          dueDate: transactionAndDueDates(daysToPay).dueDate,
+          selectedRate,
+        });
 
         res.status(200).json({
             status:200,
             userId,
             lenderId,
             loanAmount,
+            transactionDate: transactionAndDueDates(daysToPay).transactionDate,
+            dueDate: transactionAndDueDates(daysToPay).dueDate,
+            selectedRate,
+            daysToPay,
         });
         // client.close();
         // console.log("disconnected!");
