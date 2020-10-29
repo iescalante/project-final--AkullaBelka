8fb93c18c9e85c19489bc543520ee514c9ec68fd
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-28 21:24:33 -0400

    cleanup

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import MainHeader from '../reusable-components/MainHeader';
 import Container from '../reusable-components/Container';
 import { Link } from 'react-router-dom';
+import { AppContext } from '../AppContext';
 
 const Homepage = () => {
   const [profileVisible, setProfileVisible] = React.useState(false);
