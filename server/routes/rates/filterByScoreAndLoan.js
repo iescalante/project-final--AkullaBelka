f628160e1bdd470a207b12c3d5b8452eef0798d9
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-31 11:44:00 -0400

    added variables for numbers used as limits

--- server/routes/rates/filterByScoreAndLoan.js
+++ server/routes/rates/filterByScoreAndLoan.js
@@ -10,54 +10,28 @@ const options = {
 module.exports = async (req, res) => {
     const { score, loanAmount } = req.params;
     const client = await MongoClient(MONGO_URI, options);
+    const lowScore = 0;
+    const highScore = 1000;
+    const lowLoan = 100;
+    const highLoan = 1000;
     try {
       await client.connect();
       const db = client.db("akulla_belka");
       console.log("connected!");
 
-      if (score < 0 || score > 1000) {
-        res.status(400).json({ status: 400, message:"Score can only be 0 to 1000" });
+      if (score < lowScore || score > highScore) {
+        res.status(400).json({ status: 400, message:`Score can only be ${lowScore} to ${highScore}` });
         return;
       }
-      if (loanAmount < 100 || loanAmount > 1000) {
-        res.status(400).json({ status: 400, message:"Loans can only be from 100 to 1000" });
+      if (loanAmount < lowLoan || loanAmount > highLoan) {
+        res.status(400).json({ status: 400, message:`Loans can only be from ${lowLoan} to ${highLoan}` });
         return;
       }
       let query = {minScore: {$lte: Number(score)}, maxScore: {$gte:Number(score)}, minLoan: {$lte: Number(loanAmount)}, maxLoan: {$gte: Number(loanAmount)}};
       console.log(query);
+
       const availableRates = await db.collection("rates").findOne(query);
-
       res.status(200).json({availableRates});
-      // if (-1 < score < 500) {
-      //   const data = await db.collection("rates").find({ minScore: { $eq: 0 } }).toArray();
-      //   res.status(200).json({ status:200, data });
-      // }
-      // else if (score >= 500) {
-      //   let query = {
-      //     minScore: 500,
-
-      //   }
-      //   const data = await db.collection("rates").find({ minScore: { $eq: 500 }}).toArray();
-      //   if (loanAmount < 0 || loanAmount > 1000) {
-      //     res.status(400).json({ status: 400, message: "Loan can only be from 100 to 1000" });
-      //   }
-      //   if (99 < loanAmount < 401){
-      //     const lowLoanAmountRate = await db.collection("rates").find({ minLoan : { $eq: 100}}).toArray();
-      //     res.status(200).json({ status:200, lowLoanAmountRate });
-      //   }
-      //   if (400 < loanAmount < 801) {
-      //     const mediumLoanAmountRate = await db.collection("rates").find({ minLoan: { $eq: 401 }}).toArray();
-      //     res.status(200).json({ status: 200, mediumLoanAmountRate });
-      //   }
-      //   if (800 < loanAmount < 1001) {
-      //     const highLoanAmountRate = await db.collection("rates").find({ minLoan : { $eq: 801 }}).toArray();
-      //     res.status(200).json({ status:200, highLoanAmountRate });
-      //   }
-      //   if (499 < score < 1001 ) {
-          
-      //   }
-      //   // res.status(200).json({ status: 200, data });
-      // }
       
       client.close();
       console.log("disconnected!");
