c7513b1455251b65d5408cee71602a23b98ca12f
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-31 19:03:38 -0400

    created SelectedLender form component

--- client/src/components/SelectedLender.js
+++ client/src/components/SelectedLender.js
@@ -1,31 +1,33 @@
-// import React from 'react';
-// import styled from 'styled-components';
+import React from 'react';
+import styled from 'styled-components';
 
-// const SelectedRate = ({availableRate, selectedRate, setSelectedRate}) => {
-//   console.log(availableRate.rate);
-//   const selectRate = (ev) => {
-//     ev.preventDefault();
-//     setSelectedRate(ev.target.value);
-//   };
-//   return (
-//     <SingleRateForm>
-//       <InfoLine>Rate: {availableRate.rate}</InfoLine>
-//       <InfoLine>Grace Period (Days): {availableRate.daysToPayBack}</InfoLine>
-//       <SelectRateButton onClick={selectRate} type="submit" value={availableRate.rate}>Select This Rate</SelectRateButton>
-//     </SingleRateForm>
-//   )
-// };
-// const SingleRateForm = styled.form`
-//   display:flex;
-//   flex-direction:column;
-//   margin: 15px 0;
-//   flex:1;
-// `;
-// const InfoLine = styled.p`
-//   font-weight:bold;
-// `;
-// const SelectRateButton = styled.button`
-//   background:goldenrod;
-//   cursor: pointer;
-// `;
-// export default SelectedRate;
+const SelectedLender = ({lenderId, availableLoan, totalLoan, selectedLender, setSelectedLender}) => {
+  const selectLender = (ev) => {
+    ev.preventDefault();
+    setSelectedLender(ev.target.value);
+  };
+  
+  return (
+    <SingleLenderForm>
+      <InfoLine>Lender's Id: {lenderId}</InfoLine>
+      <InfoLine>How much can be lent: {availableLoan - totalLoan}</InfoLine>
+      <SelectLenderButton onClick={selectLender} type="submit" value={lenderId}>Select This Lender</SelectLenderButton>
+    </SingleLenderForm>
+  )
+};
+const SingleLenderForm = styled.form`
+  display:flex;
+  flex-direction:column;
+  margin: 15px 0;
+  flex:1;
+`;
+const InfoLine = styled.p`
+  font-weight:bold;
+`;
+const SelectLenderButton = styled.button`
+  margin: 10px auto;
+  width:fit-content;
+  background:goldenrod;
+  cursor: pointer;
+`;
+export default SelectedLender;
