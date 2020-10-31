75ff0169a9a892cfe3af200f7b4b199333cc88f8
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-31 19:02:27 -0400

    added Lenders form and final submission with fetch

--- client/src/components/GetLoan.js
+++ client/src/components/GetLoan.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 import Container from '../reusable-components/Container';
 import Header from '../reusable-components/Header';
 import AvailableRates from "./AvailableRates";
 import AvailableLenders from './AvailableLenders';
+import { AppContext } from '../AppContext';
 
 const GetLoan = () => {
   const [availableRate, setAvailableRate] = React.useState(null);
   const [loan, setLoan] = React.useState(0);
-  const [selectedRate, setSelectedRate] = React.useState(0);
+  const [selectedRate, setSelectedRate] = React.useState(null);
+  const [availableLenders, setAvailableLenders] = React.useState(null);
+  const [selectedLender, setSelectedLender] = React.useState(null);
+  const { userData } = React.useContext(AppContext);
+  const history = useHistory();
+
+  const submitLoanApplication = (ev) => {
+    ev.preventDefault();
+    const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        loanAmount: Number(loan),
+        selectedRate: Number(selectedRate),
+        daysToPay: Number(availableRate.daysToPayBack)
+        })
+  };
+  fetch(`/loans/${userData.currentUser._id}/${selectedLender}`, requestOptions)
+      .then(response => response.json())
+      .then((responseBody) => {
+        console.log("form submitted:",responseBody);
+        history.push("/main");
+      });
+  };
 
   return(
     <Container>
@@ -21,8 +46,39 @@ const GetLoan = () => {
         selectedRate={selectedRate}
         setSelectedRate={setSelectedRate}
       />
-      {/* <AvailableLenders/> */}
+      {availableRate && 
+        <AvailableLenders 
+          loan={loan}
+          availableLenders={availableLenders}
+          setAvailableLenders={setAvailableLenders}
+          selectedLender={selectedLender}
+          setSelectedLender={setSelectedLender}
+        />
+      }
+      {selectedRate && selectedLender &&
+        <SubmitLoan onSubmit={submitLoanApplication}>
+          Here is your summary of your selection, please review!
+          <InfoLine>Asking Loan: {loan}</InfoLine>
+          <InfoLine>Rate Selected :{selectedRate}</InfoLine>
+          <InfoLine>Lender Selection: {selectedLender}</InfoLine>
+          <SubmitButton type='submit'>Click here to submit everything</SubmitButton>
+        </SubmitLoan>
+      }
     </Container>
   )
 };
+const SubmitLoan = styled.form`
+  display:flex;
+  flex-direction:column;
+  text-align: center;
+`;
+const InfoLine = styled.p`
+  font-weight: bold;
+`;
+const SubmitButton = styled.button`
+  margin: 10px auto;
+  width: fit-content;
+  background: goldenrod;
+  cursor: pointer;
+`;
 export default GetLoan;
