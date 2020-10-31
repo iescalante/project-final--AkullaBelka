6c6f41071d4c3e3f0b1724d023991948e3a1affc
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-31 16:12:56 -0400

    created for 2nd part of Get a Loan page

---
+++ client/src/components/SelectedRate.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SelectedRate = ({availableRate, selectedRate, setSelectedRate}) => {
+  console.log(availableRate.rate);
+  const selectRate = (ev) => {
+    ev.preventDefault();
+    setSelectedRate(ev.target.value);
+  };
+  return (
+    <SingleRateForm>
+      <InfoLine>Rate: {availableRate.rate}</InfoLine>
+      <InfoLine>Grace Period (Days): {availableRate.daysToPayBack}</InfoLine>
+      <SelectRateButton onClick={selectRate} type="submit" value={availableRate.rate}>Select This Rate</SelectRateButton>
+    </SingleRateForm>
+  )
+};
+const SingleRateForm = styled.form`
+  display:flex;
+  flex-direction:column;
+  margin: 15px 0;
+  flex:1;
+  text-align:center;
+`;
+const InfoLine = styled.p`
+  font-weight:bold;
+`;
+const SelectRateButton = styled.button`
+  background:goldenrod;
+  cursor: pointer;
+`;
+export default SelectedRate;
