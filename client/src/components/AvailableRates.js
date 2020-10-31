087a958bdd4a2dd8b1058b26e64b78d8848d39f6
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-10-31 16:14:24 -0400

    pushed props higher to have access in GetLoan page

--- client/src/components/AvailableRates.js
+++ client/src/components/AvailableRates.js
@@ -1,60 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 import Container from '../reusable-components/Container';
-// import RateInfo from './RateInfo';
+import SelectedRate from './SelectedRate';
+import { AppContext } from '../AppContext';
+import Spinner from '../Spinner';
 
-const AvailableRates = () => {
-  const [availableRates, setAvailableRates] = React.useState([]);
-  const [loan, setLoan] = React.useState(0);
-  const [selectedRate, setSelectedRate] = React.useState(0);
+const AvailableRates = ({availableRate, setAvailableRate, selectedRate, setSelectedRate, loan, setLoan}) => {
+  const { userData } = React.useContext(AppContext);
+  const [isLoading, setIsLoading] = React.useState(false);
   const changeLoanAmount = (ev) => {
     setLoan(ev.target.value);
   };
-
-  const getRates = (ev) => {
+  const getRate = (ev) => {
     ev.preventDefault();
-    fetch(`/rates`)
+    setIsLoading(true);
+    fetch(`/rates/${userData.currentUser.score}/${loan}`)
     .then(res => res.json())
     .then(responseBody => {
-      console.log(responseBody.data)
-      setAvailableRates(responseBody.data);
+      console.log(responseBody.availableRate)
+      setAvailableRate(responseBody.availableRate);
+      setIsLoading(false);
     })
-  };
-
-  const selectRate = (ev) => {
-    ev.preventDefault();
-    setSelectedRate(ev.target.value);
-    console.log(selectedRate);
   };
 
   return(
     <Container>
-      <GetRateForm onSubmit={getRates}>
+      <GetRateForm onSubmit={getRate}>
         <GetRateLabel>
           How much money do you need?:
           <LoanInput onChange ={changeLoanAmount} type="number" name="loan" value={loan}/>
         </GetRateLabel>
         <GetRateButton type="submit">Get a Rate!</GetRateButton>
       </GetRateForm>
-      <RatesDiv>
-        {availableRates && availableRates.length > 0 && 
-          availableRates.map((rate)=>{
-            return(
-              <SingleRateForm>
-                <InfoLine>Rate: {rate.rate}</InfoLine>
-                <InfoLine>Score Required: {rate.minScore}</InfoLine>
-                <InfoLine>Minimum Loan: {rate.minLoan}</InfoLine>
-                <InfoLine>Maximum Loan: {rate.maxLoan}</InfoLine>
-                <InfoLine>Grace Period (Days): {rate.daysToPayBack}</InfoLine>
-                <SelectRateButton onClick={selectRate} type="submit" value={rate.rate}>Select This Rate</SelectRateButton>
-              </SingleRateForm>
-            )
-          })
-        }
-      </RatesDiv>
+      {isLoading ? (<Spinner/>) : (
+        <RatesDiv>
+          {availableRate && 
+            <SelectedRate 
+              availableRate={availableRate}
+              selectedRate={selectedRate}
+              setSelectedRate={setSelectedRate}
+            />
+          }
+        </RatesDiv>
+        )}
     </Container>
-  )
-};
+  )};
 const GetRateForm = styled.form`
   display: flex;
   flex-direction: column;
@@ -85,18 +75,6 @@ const GetRateButton = styled.button`
 const RatesDiv = styled.div`
   display:flex;
   font-size:0.9em;
-`;
-const SingleRateForm = styled.form`
-  display:flex;
-  flex-direction:column;
-  margin: 15px 0;
-  flex:1;
-`;
-const InfoLine = styled.p`
-  font-weight:bold;
-`;
-const SelectRateButton = styled.button`
-  background:goldenrod;
-  cursor: pointer;
+  margin:0 auto;
 `;
 export default AvailableRates;
--- client/src/components/GetLoan.js
+++ client/src/components/GetLoan.js
@@ -3,13 +3,25 @@ import styled from 'styled-components';
 import Container from '../reusable-components/Container';
 import Header from '../reusable-components/Header';
 import AvailableRates from "./AvailableRates";
+import AvailableLenders from './AvailableLenders';
 
 const GetLoan = () => {
+  const [availableRate, setAvailableRate] = React.useState(null);
+  const [loan, setLoan] = React.useState(0);
+  const [selectedRate, setSelectedRate] = React.useState(0);
+
   return(
     <Container>
       <Header pageTitle={"Loan Application Page"}/>
-      <AvailableRates/>
-      <div>This is where you apply to get a loan!</div>
+      <AvailableRates
+        availableRate={availableRate}
+        setAvailableRate={setAvailableRate}
+        loan={loan}
+        setLoan={setLoan}
+        selectedRate={selectedRate}
+        setSelectedRate={setSelectedRate}
+      />
+      {/* <AvailableLenders/> */}
     </Container>
   )
 };
