661b4dd16a6084f6e400f39b9cc98a0919167dd3
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-11-02 21:50:52 -0500

    mini tweaks

--- client/src/components/SelectedLender.js
+++ client/src/components/SelectedLender.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SelectedLender = ({lenderId, availableLoan, totalLoan, selectedLender, setSelectedLender}) => {
+const SelectedLender = ({lenderId, availableLoan, totalLoan, setSelectedLender}) => {
   const selectLender = (ev) => {
     ev.preventDefault();
     setSelectedLender(ev.target.value);
--- client/src/components/SelectedRate.js
+++ client/src/components/SelectedRate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SelectedRate = ({availableRate, selectedRate, setSelectedRate}) => {
+const SelectedRate = ({availableRate, setSelectedRate}) => {
   const selectRate = (ev) => {
     ev.preventDefault();
     setSelectedRate(ev.target.value);
