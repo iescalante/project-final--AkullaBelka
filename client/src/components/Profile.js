607869525f59edfddf824a7dad36f02d29f4cd67
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-11-02 21:50:09 -0500

    added Lender portion of profile when Yes

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -16,9 +16,16 @@ const Profile = () => {
         <UserInfo>LAST NAME : {userData.currentUser.lastName}</UserInfo>
         <UserInfo>E-MAIL : {userData.currentUser.email}</UserInfo>
         <UserInfo>SCORE : {userData.currentUser.score}</UserInfo>
-        <UserInfo>TOTAL LOANED : {userData.currentUser.totalLoaned}</UserInfo>
-        <UserInfo>LOAN LIMIT : {userData.currentUser.loanLimit}</UserInfo>
+        <UserInfo>TOTAL LOANED ($) : {userData.currentUser.totalLoaned}</UserInfo>
+        <UserInfo>LOAN LIMIT ($) : {userData.currentUser.loanLimit}</UserInfo>
         <UserInfo>Lender? {userData.currentUser.lenderProfile.lender ? "Yes" : "No"}</UserInfo>
+        {userData.currentUser.lenderProfile.lender && 
+          (
+            <>
+              <UserInfo>How much has been lent ($): {userData.currentUser.lenderProfile.totalLoan}</UserInfo>
+              <UserInfo>Available loan to lend ($): {userData.currentUser.lenderProfile.availableLoan}</UserInfo>
+            </>
+          )}
       </ProfileDiv>
     </Container>
   )
