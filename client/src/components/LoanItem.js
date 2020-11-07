ced9b03231f721103229d8d3d1d37dfd585dac56
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-11-06 22:46:20 -0500

    fetchd data from loans and mappd items using props

---
+++ client/src/components/LoanItem.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LoanItem = ({loanId,transactionDate,dueDate,loanAmount,selectedRate,paidAmount,lenderId,setSelectedLoan}) => {
+  const handleSelectedLoan = (ev) => {
+    ev.preventDefault();
+    setSelectedLoan({
+      loanId,
+      transactionDate,
+      dueDate,
+      loanAmount,
+      selectedRate,
+      lenderId
+    })
+  };
+
+  return (
+    <SingleLoanDiv>
+      <LoanInfoList>
+        <LoanInfo>Transaction Date: {transactionDate}</LoanInfo>
+        <LoanInfo>Due Date: {dueDate}</LoanInfo>
+        <LoanInfo>Loan Amount: {loanAmount}</LoanInfo>
+        <LoanInfo>Selected rate for loan: {selectedRate}</LoanInfo>
+        <LoanInfo>Paid amount to date: {paidAmount}</LoanInfo>
+      </LoanInfoList>
+      <SelectLoanButton onClick={handleSelectedLoan}>Select this loan to pay</SelectLoanButton>
+    </SingleLoanDiv>
+  )
+};
+const SingleLoanDiv = styled.div`
+  display: flex;
+  margin: 15px auto;
+`;
+const LoanInfoList = styled.ul`
+  flex: 3;
+`;
+const LoanInfo = styled.li`
+  font-weight: bold;
+`;
+const SelectLoanButton = styled.button`
+  flex: 1;
+  height: 50px;
+  margin: auto;
+  width: 300px;
+  background: goldenrod;
+  cursor: pointer;
+`;
+export default LoanItem;
--- client/src/components/PendingLoans.js
+++ client/src/components/PendingLoans.js
@@ -1,56 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import Container from '../reusable-components/Container';
+import LoanItem from './LoanItem';
 
-const PendingLoans = ({selectedLoan, setSelectedLoan}) => {
+const PendingLoans = ({selectedLoan, setSelectedLoan, loanData}) => {
   return(
     <Container>
-      <SingleLoanDiv>
-        <LoanInfoList>
-          <LoanInfo>One info</LoanInfo>
-          <LoanInfo>Two info</LoanInfo>
-          <LoanInfo>Three info</LoanInfo>
-          <LoanInfo>Four info</LoanInfo>
-        </LoanInfoList>
-        <SelectLoanButton>Select this loan to pay</SelectLoanButton>
-      </SingleLoanDiv>
-      <SingleLoanDiv>
-        <LoanInfoList>
-          <LoanInfo>One info</LoanInfo>
-          <LoanInfo>Two info</LoanInfo>
-          <LoanInfo>Three info</LoanInfo>
-          <LoanInfo>Four info</LoanInfo>
-        </LoanInfoList>
-        <SelectLoanButton>Select this loan to pay</SelectLoanButton>
-      </SingleLoanDiv>
-      <SingleLoanDiv>
-        <LoanInfoList>
-          <LoanInfo>One info</LoanInfo>
-          <LoanInfo>Two info</LoanInfo>
-          <LoanInfo>Three info</LoanInfo>
-          <LoanInfo>Four info</LoanInfo>
-        </LoanInfoList>
-        <SelectLoanButton>Select this loan to pay</SelectLoanButton>
-      </SingleLoanDiv>
+      {loanData.map((loan)=> {
+        return (
+          <LoanItem
+            loanId={loan._id}
+            transactionDate={loan.transactionDate}
+            dueDate={loan.dueDate}
+            loanAmount={loan.loanAmount}
+            selectedRate={loan.selectedRate}
+            paidAmount={loan.paidAmount}
+            lenderId={loan.lenderId}
+            setSelectedLoan={setSelectedLoan}
+          />)
+      })}
     </Container>
   );
 };
-const SingleLoanDiv = styled.div`
-  display: flex;
-  margin: 15px auto;
-`;
-const LoanInfoList = styled.ul`
-  flex: 3;
-`;
-const LoanInfo = styled.li`
-  font-weight: bold;
-`;
-const SelectLoanButton = styled.button`
-  flex: 1;
-  height: 50px;
-  margin: auto;
-  width: 300px;
-  background: goldenrod;
-  cursor: pointer;
-`;
 export default PendingLoans;
