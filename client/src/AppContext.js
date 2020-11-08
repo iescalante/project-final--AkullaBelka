46efdb222288c108dfedbfe56c1485f3ef4265ff
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-11-08 18:45:39 -0500

    added 2 new states for confirmation

--- client/src/AppContext.js
+++ client/src/AppContext.js
@@ -7,10 +7,23 @@ export const AppProvider = ({children}) => {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
     const [userData, setUserData] = React.useState({currentUser:{}});
+    const [loanConfirmation, setLoanConfirmation] = React.useState(null);
+    const [paymentConfirmation, setPaymentConfirmation] = React.useState(null);
 
     return (
-        <AppContext.Provider value={{ email,setEmail,password, setPassword,userData, setUserData}}>
-            {children}
+        <AppContext.Provider value={{ 
+            email,
+            setEmail,
+            password, 
+            setPassword,
+            userData,
+            setUserData,
+            loanConfirmation,
+            setLoanConfirmation,
+            paymentConfirmation,
+            setPaymentConfirmation
+        }}>
+          {children}
         </AppContext.Provider>
     )
 };
