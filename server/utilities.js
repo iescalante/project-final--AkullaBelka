380e42874bbd6d9e384baca58d5af9934eef5631
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-11-09 20:24:12 -0500

    modified to show days passed till due date

--- server/utilities.js
+++ server/utilities.js
@@ -1,12 +1,16 @@
-const transactionAndDueDates = (days) => {
+const transactionAndDueDates = (days, payDay) => {
   let dueTime = 1000*60*60*24*days;
   let startDate = new Date();
   let endDate = new Date(startDate.getTime() + dueTime).toUTCString();
-
+  let currentDate = Date.parse(new Date());
+  let daysPassed = (Date.parse(payDay)- currentDate)/(1000*60*60*24);
+  
   return { 
     transactionDate: startDate.toUTCString(),
-    dueDate: endDate
+    dueDate: endDate,
+    currentDate,
+    daysPassed
   };
 }
-
+// console.log(transactionAndDueDates(20,'Sun, 29 Nov 2020 01:18:33 GMT' ));
 module.exports = { transactionAndDueDates };
