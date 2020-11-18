aef21d608fae7c8bfd9666e318b83251f40fd4f5
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-11-17 21:23:34 -0500

    created as reusable comp

---
+++ client/src/reusable-components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <>
+      <Copyright>AkullaBelka © 2020</Copyright>
+    </>
+  )
+};
+const Copyright = styled.div`
+  margin: 15px auto;
+`;
+export default Footer;
